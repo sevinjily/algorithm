9cb796443071a80324a2268ac63caa3e060cc576
svncmsva <sevincmusaeva01@gmail.com>
2023-10-12 22:19:05 +0400

    first algorithm commit

---
+++ 04.10.23/js/main.js
@@ -0,0 +1,53 @@
+'use strict';
+// sual 1
+// const eded=prompt("Bir eded daxil edin:");
+// const number=parseInt(eded);
+// if(number>1000 && number<100000 &&number%3==0 ){
+
+// console.log("eded: "+number);
+//         alert("eded 3 e bolunur");
+    
+// }
+// else{
+//    console.log("eded: "+number);
+//     alert("eded 3 e bolunmur");
+// }
+// sual2
+// const user={
+//     name:'Elnur',
+//     height: 179,
+//     phone:{
+//         model:'Iphone',
+//     },
+//     orders:['book','mouse','mousepad']
+//     }
+   
+// a)
+// ++user.height;
+
+// b)
+// user.phone.model='Samsung';
+// c)
+// user.phone. series='Galaxy s22';
+
+// d)
+// delete user.name;
+
+// e)
+// delete user.orders[0];
+
+// f)
+// delete user.orders[user.orders.length-1];
+
+// g)
+// user.orders.unshift('ball');
+
+// h)
+// user.orders.push('headphones');
+// i)
+// console.log(user);
+// sual 3
+const info=["Resul","Serifov",35];
+const conCat=info[0]+" "+info[1];
+const new_arr=[ conCat,info[info.length-1]];
+console.log(new_arr);
